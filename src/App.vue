b518ccedd625669f9f38bb09e8233126e08618b0
Akeju Paul <akejupaul10@gmail.com>
2021-10-07 21:47:45 +0100

    make it

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@
   <div style="height:100%;margin:0;margin-left:-16px" class="mt-n2" >
    <v-row align="center" justify="center"  no-gutters class=" mt-1 pa-3" style="height:100%;width:150px;margin:0">
      <p><v-icon color="#F64B6B">mdi-baguette</v-icon></p>
-     <p class="ml-1" style="font-weight:bold;font-size:20px;color:#F64B6B">esweet.</p>
+     <p class="ml-1" style="font-weight:bold;font-size:20px;color:#F64B6B">belleful.</p>
    </v-row>
   </div>
   <v-spacer></v-spacer>
