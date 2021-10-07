7ca680e45810e91831b30c5ab1db7b0d932a55c4
Akeju Paul <akejupaul10@gmail.com>
2021-10-07 22:53:36 +0100

    make it better

--- src/views/Category.vue
+++ src/views/Category.vue
@@ -3,19 +3,21 @@
       <v-container>
           <div style="margin-top:10vh"></div>
       <p class="text-h4 font-weight-bold ma-4">Explore by <br>category</p>
-
-     <div style="height:70vh;overflow-y:scroll" class="pa-2">
+        <div>
+            <v-text-field rounded append-icon="mdi-magnify" @click:append="searchit" dense solo class="d-flex d-sm-none ml-4 mr-4 mb-n3 text-caption font-weight-bold" placeholder="Search for your favourite"></v-text-field>
+        </div>
+     <div style="height:60vh;overflow-y:scroll" class="pa-2">
           <v-row no-gutters >
           <v-col class="pa-3" v-for="i in 12" :key="i" cols="12" sm="6" lg="3" xl="3" >
               <v-card @click="dialog=true" hover  color="">
-                  <v-img height="130" src="../assets/bb.jpg"></v-img>
-                  <div class="pa-2">
-                      <p class="font-weight-bold">Solid Bagel</p>
-                      <p class="text-caption mt-n3">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat dicta nulla in.</p>
-                      <v-row align="center" justify="center" no-gutters>
-                          <v-btn text class="text-caption" style="text-transform:none" plain>Add to Cart <v-icon size="15" class="ml-1">mdi-cart-plus</v-icon></v-btn>
+                  <v-img height="250" src="../assets/bb.jpg"></v-img>
+                  <div style="position:absolute;bottom:0;width:95%" class="white ma-2 pa-2">
+                     
+                      <p class="text-caption font-weight-bold text-right mb-n1" style="color:orange">$40.50</p>
+                      <v-row align="center" class="mb-n3" justify="center" no-gutters>
+                          <p class="font-weight-bold mt-2">Solid Bagel</p>
                           <v-spacer></v-spacer>
-                          <v-btn icon><v-icon>mdi-arrow-right</v-icon></v-btn>
+                          <v-btn class="mt-n2" icon><v-icon>mdi-arrow-right</v-icon></v-btn>
                       </v-row>
 
                   </div>
@@ -33,8 +35,11 @@
       <v-card class="white" flat>
           <v-row no-gutters style="height:60vh">
               <v-col cols="12" xl="6" lg="6">
-                <v-img class="d-none d-sm-flex" height="60vh" src="../assets/bb.jpg"></v-img>
+                <div class="d-sm-none d-md-flex">
+                    <v-img class="d-none d-sm-flex" height="60vh" src="../assets/bb.jpg"></v-img>
+                </div>
                 <v-img class="d-flex d-sm-none" height="30vh" src="../assets/bb.jpg"></v-img>
+                <v-img class="d-none d-sm-flex d-md-none" height="40vh" src="../assets/bb.jpg"></v-img>
               </v-col>
               <v-col cols="12" xl="6" lg="6">
                 <div class="pa-2">
@@ -53,7 +58,21 @@
     ></v-rating>
     <p style="font-size:10px"><span style="color:orange">3.5k</span> reviews</p>
                     <p class="text-caption mt-n2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi consectetur natus sequi, fugiat doloribus ea dolore sint cupiditate. Ipsam, libero.</p>
-                     <v-btn text class="text-caption mt-n3 ml-n4" style="text-transform:none" plain>Add to Cart <v-icon size="15" class="ml-1">mdi-cart-plus</v-icon></v-btn>
+                     <v-btn block outlined class="text-caption mt-n3 " style="text-transform:none" plain>Add to Cart <v-icon size="15" class="ml-1">mdi-cart-plus</v-icon></v-btn>
+                     <div class="d-none d-sm-flex">
+                         <div style="width:100%" class="d-sm-none d-md-flex mt-2">
+                             <div style="width:100%">
+                                 <p style="font-weight:bold" class="text-body-2">Reviews</p>
+                            <v-row style="width:100%"  no-gutters>
+                                <v-spacer></v-spacer>
+                                 <p><v-icon size="70" color="grey lighten-2">mdi-delete-empty-outline</v-icon></p>
+                                 <v-spacer></v-spacer>
+                            </v-row>
+                            <p class="text-center text-caption grey--text">No review yet for this product</p>
+                             </div>
+                         </div>
+                     </div>
+                     
                 </div>
               </v-col>
           </v-row>
@@ -67,6 +86,11 @@ export default {
     data(){
         return {
             dialog:false
+        }
+    },
+    methods:{
+        searchit(){
+            alert("searching")
         }
     }
 }
