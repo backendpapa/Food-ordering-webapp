4546cfe831a4c11b9656c1a4bba8160b38ac2465
Akeju Paul <akejupaul10@gmail.com>
2021-10-08 20:02:40 +0100

    make it better

--- src/views/OrderDetail.vue
+++ src/views/OrderDetail.vue
@@ -47,6 +47,36 @@
                  </div>
                  <div  v-else style="height:40vh;width:100%;box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);position:absolute;bottom:0;" class="white pa-2 animate__animated animate__fadeIn ">
                     <v-btn icon @click="toggle=!toggle"  plain> <v-icon>mdi-close</v-icon></v-btn>
+                    <div class="pa-2">
+                        <p class="text-h4">Order#58392</p>
+                        <p class="mt-n4"><i>In transit</i><v-icon color="pink" size="20">mdi-taxi</v-icon></p>
+                        <p class="text-body-2 font-weight-bold mt-n3">45 min</p>
+                        <p style="font-size:11px" class="mt-n4">Arrival time(approx).</p>
+                        <p class="text-caption mt-n3">Bagel,Grilled Chicken,Malt,Salad</p>
+                    </div>
+                    <v-row no-gutters align="center" justify="center" class="pa-2">
+                         <div>
+                             <v-rating
+      v-model="rating"
+      background-color="grey"
+      color="black"
+      dense
+      small
+      large
+    ></v-rating>
+    
+    <p class=" mb-2" style="font-size:7px">Rate now!</p>
+                         </div>
+                        <v-spacer></v-spacer>
+                        <div>
+                            <v-btn plain  icon><v-icon>mdi-phone</v-icon></v-btn>
+                            <p class=" mt-n1" style="font-size:7px">Report</p>
+                        </div>
+                        <div>
+                            <v-btn plain  icon><v-icon>mdi-close-circle-multiple-outline</v-icon></v-btn>
+                            <p class=" mt-n1" style="font-size:7px">Cancel order</p>
+                        </div>
+                    </v-row>
              </div>
              </div>
          </v-col>
